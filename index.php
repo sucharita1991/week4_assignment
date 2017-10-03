67d93dd886b981e68b8c036b3db0b9a7801fc954
Sucharita Das <sd686@afsconnect2.njit.edu>
2017-10-03 00:25:42 -0400

    date compare

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
-$date =  date('Y-m-d', time());
+$date =  date('Y/m/d', time());
 echo "The value of \$date: ".$date."<br>";
-
-$date1 =  date('Y/m/d', time());
-echo "The value of \$date1: ".$date1."<br>";
 
 $tar = "2017/05/24";
 echo "The value of \$tar: ".$tar."<br>";
+
+$compareDates = strcmp($date,$tar);
+if($compareDates == 0){
+	echo "Oops</br>";
+} else if($compareDates > 0){
+	echo "The future</br>";
+} else {
+	echo "The past</br>";
+} 
+
 
 $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
