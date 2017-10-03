fa580fe90a496ea959bf83cfd9eafc0210d0b7f8
Sucharita Das <sd686@afsconnect2.njit.edu>
2017-10-03 01:23:26 -0400

    ascii of first character of string

--- index.php
+++ index.php
@@ -33,8 +33,10 @@ echo str_word_count($date)."</br>";
 $string1 = "My name is Sucharita Das";
 echo "The length of string: ".$string1." is ".strlen($string1)."</br>";
 
+echo "The Ascii code of first character of string: ".$string1." is: ".ord(substr($string1, 0,1))."</br>";
+
 $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
 print_r($year)
 
-?>
+?>  
