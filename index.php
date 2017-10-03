6169c2114aed74aea0272fcc9ae599c7c87b2708
Sucharita Das <sd686@afsconnect2.njit.edu>
2017-10-03 01:02:20 -0400

    position of / in date

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ echo "The value of \$date: ".$date."<br>";
 $tar = "2017/05/24";
 echo "The value of \$tar: ".$tar."<br>";
 
+
+echo "Compare Dates: </br>";
 $compareDates = strcmp($date,$tar);
 if($compareDates == 0){
 	echo "Oops</br>";
@@ -14,6 +16,18 @@ if($compareDates == 0){
 	echo "The past</br>";
 } 
 
+echo "Positions of / in date</br>";
+$lastPos = 0;
+$positions = array();
+while (($lastPos = strpos($date, "/", $lastPos))!== false) {
+    $positions[] = $lastPos;
+    $lastPos = $lastPos + 1;
+}
+
+foreach ($positions as $value) {
+    echo $value ."&nbsp;";
+}
+echo "</br>";
 
 $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
